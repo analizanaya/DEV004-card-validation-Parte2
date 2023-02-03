032327707eb7e050308c0ec939b513a40c12e90b
analizanaya <lizinayari@gmail.com>
2023-02-03 06:28:31 -0500

    Proyect 1 B

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,53 @@
-const validator = {
-  // ...
-};
+ function validate(numeroIngresado) {
 
+  const deStringaArray = numeroIngresado.split(''); //".split" - Cambiar un string a array.
+  const numeroReversa = deStringaArray.reverse(); //".reverse" - Array al revés.
+  let reverse = numeroReversa.join(''); // ".join" - Cambiar un array a string.
+
+  let resultado = 0;
+  let elem = null;
+  let alpha = null;
+
+  for (let i = 1; i < reverse.length; i += 2) {
+    elem = parseInt(reverse.charAt(i)) * 2; //number
+
+    if (elem > 9) {
+      alpha = elem.toString();
+      elem = Number(alpha.charAt(0)) + Number(alpha.charAt(1)); //number 
+    }
+    resultado = resultado + elem;
+  }
+
+  for (let i = 0; i < reverse.length; i += 2) {
+    resultado = resultado + parseInt(reverse.charAt(i));
+  }
+  if ((resultado % 10) === 0) {
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+
+function maskify(ocultarNumero) {
+  if(ocultarNumero.length < 4){
+    return ocultarNumero
+  }
+  else{
+    let last4 = '';
+    for(let i=ocultarNumero.length - 4; i < ocultarNumero.length; i++){
+      last4 = last4 + ocultarNumero[i];
+    }
+    let mask = '';
+    for( let j=0; j<ocultarNumero.length - 4; j++){
+      mask = mask + '#';
+    }
+    return mask + last4
+  }
+}
+
+const validator = {validate, maskify};
+  
 export default validator;
+
